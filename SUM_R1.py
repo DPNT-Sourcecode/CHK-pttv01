3f999dd1c8aaebf7a9d7947603153f9b44ebf68e
sbx_user1051 <sbx_user1051@169.254.129.145>
2019-06-20 09:48:52 +0000

    Thu Jun 20 09:48:52 UTC 2019

--- SUM_R1.py
+++ SUM_R1.py
@@ -14,3 +14,9 @@ def sum(val1, val2):
         Integer : Sum of val1 and val2.
     '''
     return val1 + val2;
+
+if __name__ == '__main__':
+   # Tests.
+   print("1 + 3 = {}".format(sum(1,3)))
+   print("0 + 0 = {}".format(sum(0,0)))
+
