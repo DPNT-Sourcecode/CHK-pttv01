127a888fb5e080938a060100bba1772090f7d126
sbx_user1051 <sbx_user1051@169.254.129.145>
2019-06-20 09:56:41 +0000

    Thu Jun 20 09:56:41 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,19 @@
+#!/usr/local/bin/python3
+
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    '''
+    Function returning the sum of two parameters.
+    Args:
+        val1 : Integer between 0 and 100.
+        val2 : Integer between 0 and 100.
+    Return:
+        Integer : Sum of val1 and val2.
+    '''
+    return val1 + val2;
+
+if __name__ == '__main__':
+   # Tests.
+   print("1 + 3 = {}".format(sum(1,3)))
+   print("0 + 0 = {}".format(sum(0,0)))
+
