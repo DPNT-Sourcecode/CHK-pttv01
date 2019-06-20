1da4ace56b0725c48011206d298ce802d91ee3b2
sbx_user1051 <sbx_user1051@169.254.129.145>
2019-06-20 12:27:14 +0000

    Thu Jun 20 12:27:14 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -66,7 +66,11 @@ def checkout(skus):
        Integer : Total checkout value of the items or -1.
     '''
     # Our basket.
-    basket = { 'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F' : 0 }
+    basket = { 'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F' : 0 
+              , 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L' : 0 
+              , 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R' : 0 
+              , 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X' : 0 
+              , 'Y': 0, 'Z': 0 }
     # We parse the string to be sure every SKU is an existing letter.
     for one_sku in skus:
         if one_sku not in basket:
@@ -145,5 +149,6 @@ if __name__ == "__main__":
 #    res = checkout(my_string)
 #    print("Test for {} :: {}".format(my_string, str(res)))
         
+
 
 
