3289018a58446914ea0e70b3b784560b556f332e
sbx_user1051 <sbx_user1051@169.254.129.145>
2019-06-20 09:45:52 +0000

    Thu Jun 20 09:45:52 UTC 2019

---
+++ SUM_R1.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+
+
+# Define a function that receives two parameters
+# and return the sum. The parameters should be 
+# between 0 and 100.
+def sum(val1, val2):
+    '''
+    Function returning the sum of two parameters.
+    Args:
+        val1 : Integer between 0 and 100.
+        val2 : Integer between 0 and 100.
+    Return:
+        Integer : Sum of val1 and val2.
+    '''
+    return val1 + val2;
