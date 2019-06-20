d3ff2b242349f07d87542d53e5c3bde728441825
sbx_user1051 <sbx_user1051@169.254.129.145>
2019-06-20 10:05:17 +0000

    Thu Jun 20 10:05:17 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,6 +1,19 @@
-
+#!/usr/local/bin/python3
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    raise NotImplementedError()
+    ''' 
+    Method returning a string saying hello to 
+    the name provided as parameter.
+    Arg:
+        friend_name : Name to say hello to.
+    Return:
+        String saying : Hello friend_name.
+    '''
+    return "Hello " + frien_name;
+
+if __name__ == '__main__':
+    print("{}".format(hello("Bruno")))
+    print("{}".format(hello("")))
+
