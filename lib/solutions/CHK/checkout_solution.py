84c545657002fb7400c88ed44b5a8cc5b21a2a89
sbx_user1051 <sbx_user1051@169.254.129.145>
2019-06-20 13:36:46 +0000

    Thu Jun 20 13:36:46 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -66,7 +66,7 @@ def process_any(basket):
     idx_price = 0
     idx_item = 0
     counter = 0
-    while counter < num_of_items:
+    while counter < num_of_groups * any_of_them['number']:
        if basket[sorted_dict_of_items[sorted_elems[idx_price]][idx_item]] == 0:
            idx_item += 1
            if idx_item >= len(sorted_dict_of_items[sorted_elems[idx_price]]):
@@ -196,6 +196,7 @@ if __name__ == "__main__":
 #    res = checkout(my_string)
 #    print("Test for {} :: {}".format(my_string, str(res)))
         
+
 
 
 
